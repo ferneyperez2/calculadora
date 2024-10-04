24531ccb8179778a6807fa0aa64af925962677b1
ferneyperez2 <127696661+ferneyperez2@users.noreply.github.com>
2024-10-04 16:20:44 -0500

    Add files via upload

---
+++ calculadora.js
@@ -0,0 +1,15 @@
+/*
+  Plantilla: HunigoleSan
+*/
+/*        PROBLEMA: Programa tu calculadora */
+/* 
+   Deberás cumplir todas las operaciones básicas: suma, resta, multiplicación, división,
+   también debe permitir limpiar el resultado.
+
+   PROPÓSITO: esta tarea consiste en lograr conseguir el objetivo para desarrollar la lógica de programación
+   MISIÓN: Aplicar todos los conocimientos que tienes sobre JavaScript para lograr el objetivo
+   VISIÓN: Comprometete a terminar la tarea, para confirmar tu propia determinación, hacia tus metas.
+
+   NOTA: Dependerá de ti, que tan lejos desees llevar la funcionalidad de la calculadora
+   PSDT: Si crees que es necesario cambiar la estructura del HTML y CSS, puedes hacerlo con gusto.
+   */
